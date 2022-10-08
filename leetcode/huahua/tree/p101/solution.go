6ef23028668857383f0d7f03e00ac50190433dab
yinyajun-hj <yinyajun-hj@huafang.com>
2022-10-08 18:06:42 +0800

    update

--- leetcode/huahua/tree/p101/solution.go
+++ leetcode/huahua/tree/p101/solution.go
@@ -14,37 +14,37 @@ type TreeNode struct {
 // 转化为判断中序遍历结果是否是回文
 // 这个方法能通过（195/198）个case
 // 遇到这种情况就无能为力了: 【1 2 2 2 # 2 #】，因为中序遍历结果并没有完全保留二叉树的结构信息
-func isSymmetric(root *TreeNode) bool {
-	if root == nil {
-		return true
-	}
-
-	var recurse func(node *TreeNode)
-	var ser []int
-
-	recurse = func(node *TreeNode) {
-		if node == nil {
-			return
-		}
-
-		recurse(node.Left)
-		ser = append(ser, node.Val)
-		recurse(node.Right)
-	}
-
-	recurse(root)
-	// 判断ser是否是奇数长度且是回文
-	if len(ser)%2 == 0 {
-		return false
-	}
-
-	for i := 0; i < len(ser)/2; i++ {
-		if ser[i] != ser[len(ser)-1-i] {
-			return false
-		}
-	}
-	return true
-}
+//func isSymmetric(root *TreeNode) bool {
+//	if root == nil {
+//		return true
+//	}
+//
+//	var recurse func(node *TreeNode)
+//	var ser []int
+//
+//	recurse = func(node *TreeNode) {
+//		if node == nil {
+//			return
+//		}
+//
+//		recurse(node.Left)
+//		ser = append(ser, node.Val)
+//		recurse(node.Right)
+//	}
+//
+//	recurse(root)
+//	// 判断ser是否是奇数长度且是回文
+//	if len(ser)%2 == 0 {
+//		return false
+//	}
+//
+//	for i := 0; i < len(ser)/2; i++ {
+//		if ser[i] != ser[len(ser)-1-i] {
+//			return false
+//		}
+//	}
+//	return true
+//}
 
 func isSymmetric1(root *TreeNode) bool {
 	if root == nil {
--- leetcode/huahua/tree/p572/solution.go
+++ leetcode/huahua/tree/p572/solution.go
@@ -38,16 +38,80 @@ func check(a, b *TreeNode) bool {
 }
 
 // 将两棵树序列化，通过判断子串
-// 和P101一样，单纯的前序遍历会不保留全部二叉树结构信息，需要额外引入
-func isSubtree(root *TreeNode, subRoot *TreeNode) bool {
-
+// 和P101一样，单纯的前序遍历会不保留全部二叉树结构信息，需要额外引入信息，保证遍历结果的唯一性
+func isSubtree2(root *TreeNode, subRoot *TreeNode) bool {
+	// 判断sl是否是rl的子序列：kmp
+	return kmp(preorder(root), preorder(subRoot))
 }
 
 func preorder(root *TreeNode) []int {
 	var res []int
+	var recurse func(node *TreeNode)
+	const (
+		lNil = 2 << 31
+		rNil = 2<<31 + 1
+	)
 
-	if root == nil {
-		return res
+	recurse = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+
+		res = append(res, node.Val)
+		if node.Left == nil {
+			res = append(res, lNil)
+		}
+		if node.Right == nil {
+			res = append(res, rNil)
+		}
+
+		recurse(node.Left)
+		recurse(node.Right)
 	}
 
+	recurse(root)
+	return res
+}
+
+func kmp(text, pattern []int) bool {
+	// 求解next数组
+	// next[i] = pre   -->  pattern[0...pre-1] = pattern[i-pre...i-1]
+	// if pattern[pre] == pattern[i]
+	// 		next[i+1] = pre+1 = next[i] + 1
+	// else
+	// 		for pattern[i] != pattern[pre] {pre = next[pre] }
+	//      next[i+1] = next[pre] + 1
+	getNext := func(pattern []int) []int {
+		var pre int
+		var next = make([]int, len(pattern)+1)
+
+		// 该定义下，next数组前2位必然是0
+		for i := 1; i < len(pattern); i++ {
+			for pattern[i] != pattern[pre] && pre != 0 { // 不停地寻找可以和pattern[i]组成的最大公共前后缀
+				pre = next[pre]
+			}
+			// pre == 0 || pattern[i] == pattern[pre]
+			if pattern[pre] == pattern[i] {
+				pre += 1
+			}
+			next[i+1] = pre
+		}
+		return next
+	}
+
+	next := getNext(pattern)
+	var j int // pattern 指针
+	for i := 0; i < len(text); i++ {
+		for pattern[j] != text[i] && j > 0 { // 查询next数组，只改变pattern的起点
+			j = next[j]
+		}
+
+		if pattern[j] == text[i] {
+			j++
+		}
+		if j == len(pattern) {
+			return true
+		}
+	}
+	return false
 }
